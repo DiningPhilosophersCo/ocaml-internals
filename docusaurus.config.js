bd46d1106786849623d1d9fc91f6f2a7aeb7abd4
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-04-24 08:06:02 +0530

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,93 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "OCaml Internals",
+  tagline: "Notes about OCaml compiler's internals",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://ocaml-internals.dining-philosophers.dev",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "DiningPhilosophersCo", // Usually your GitHub org/user name.
+  projectName: "ocaml-internals", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/DiningPhilosophersCo/ocaml-internals/tree/main/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/ocaml-internals-social-card.jpg",
+      navbar: {
+        title: "OCaml Internals",
+        logo: {
+          alt: "Dining Philosophers Logo",
+          src: "img/logo.png",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
+          },
+          {
+            href: "https://github.com/DiningPhilosophersCo/ocaml-internals",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [],
+        copyright: `Creative Commons (CC BY-NC-ND). Dining Philosophers, LLP. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
